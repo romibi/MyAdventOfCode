78e86853210b8c10b6eefd99a63730173abe0d50
romibi <git@romibi.ch>
2025-12-01 22:28:44 +0100

    Day 1 Puzzle 2 Solved

--- 2025/day_01.py
+++ 2025/day_01.py
@@ -1,8 +1,11 @@
+from math import floor
+
 DO_LOG = True
 PUZZLE_NR = 2
 
 def main():
     #input = read_input('day_01_puzzle_1_input_small.txt')
+    #input = read_input('day_01_puzzle_1_input_small_custom.txt')
     input = read_input('day_01_puzzle_1_input.txt')
 
     dial = 50
@@ -27,25 +30,31 @@ def handle(line, dial, password):
     int_str = line[1:]
     rot = int(int_str)
 
-    started_at_0 = dial==0  # only used for PUZZLE_NR == 2
+    # dial_old = dial  # only used for PUZZLE_NR == 2
+    add_to_rot = 0   # only used for PUZZLE_NR == 2
 
     if line[0] == 'L':
+        add_to_rot = (100-dial) % 100  # only used for PUZZLE_NR == 2t
         dial -= rot
     elif line[0] == 'R':
+        add_to_rot = dial # only used for PUZZLE_NR == 2t
         dial += rot
     else:
         print('!!!!!!!!!!!!!!!!! INVALID INPUT !!!!!!!!!!!!!!!!')
 
     additional_log = ""
-    if (PUZZLE_NR == 2) and ((not started_at_0) and (dial > 100 or dial < 0)):
-        password += 1
-        additional_log = f"; during this rotation it points at 0 once"
+    if (PUZZLE_NR == 2):
+        passed_zero_times = floor((rot+add_to_rot)/100)
+        password += passed_zero_times
+        #log(f"dial: {dial_old} → {dial}, rot: {rot}, add_to_rot: {add_to_rot}, extended_rot: {rot+add_to_rot}, passed 0: {(rot+add_to_rot)/100} times?")
+        if passed_zero_times > 0:
+            additional_log = f"; during this rotation it points at zero {passed_zero_times} time(s)"
 
     dial = dial % 100
 
     log(f"The dial is rotated {line.rstrip()} to point at {dial}{additional_log}.")
 
-    if dial == 0:
+    if (PUZZLE_NR == 1) and (dial == 0):
         password += 1
 
     return dial, password
