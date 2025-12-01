1e4b00b52ead281097be4f156918c0dfc2171548
romibi <git@romibi.ch>
2025-12-01 22:08:35 +0100

    Day 1 Puzzle 2 Attempt 2

--- 2025/day_01.py
+++ 2025/day_01.py
@@ -27,6 +27,8 @@ def handle(line, dial, password):
     int_str = line[1:]
     rot = int(int_str)
 
+    started_at_0 = dial==0  # only used for PUZZLE_NR == 2
+
     if line[0] == 'L':
         dial -= rot
     elif line[0] == 'R':
@@ -34,12 +36,14 @@ def handle(line, dial, password):
     else:
         print('!!!!!!!!!!!!!!!!! INVALID INPUT !!!!!!!!!!!!!!!!')
 
-    if (PUZZLE_NR == 2) and (dial > 99 or dial < 0):
+    additional_log = ""
+    if (PUZZLE_NR == 2) and ((not started_at_0) and (dial > 100 or dial < 0)):
         password += 1
+        additional_log = f"; during this rotation it points at 0 once"
 
     dial = dial % 100
 
-    log(f"The dial is rotated {line.rstrip()} to point at {dial}.")
+    log(f"The dial is rotated {line.rstrip()} to point at {dial}{additional_log}.")
 
     if dial == 0:
         password += 1
