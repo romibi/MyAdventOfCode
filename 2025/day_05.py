fd48739a51938ef5e2bc26160721fe32510b2b4c
romibi <git@romibi.ch>
2025-12-05 21:09:07 +0100

    Day 5 Puzzle 2 Solved

--- 2025/day_05.py
+++ 2025/day_05.py
@@ -1,6 +1,8 @@
 from typing import List
 
-PUZZLE_NR=1
+from numpy import Infinity
+
+PUZZLE_NR=2
 
 
 class FreshRange:
@@ -16,11 +18,17 @@ def main():
     # input = read_input('day_05_puzzle_1_input_small.txt')
     input = read_input('day_05_puzzle_1_input.txt')
 
-    fresh_ranges, ingredients = parse_input(input)
-    fresh_count = test_ingredients(fresh_ranges, ingredients)
+    fresh_ranges, ingredients, lowest_id, higest_id = parse_input(input)
 
-    print("")
-    print(f"Of the available ingredients there are {fresh_count} fresh items.")
+    if PUZZLE_NR == 1:
+        fresh_count = test_ingredients(fresh_ranges, ingredients)
+
+        print("")
+        print(f"Of the available ingredients there are {fresh_count} fresh items.")
+    else:
+        possible_fresh_count = count_fresh_ids(fresh_ranges, lowest_id, higest_id)
+        print("")
+        print(f"With the given ranges there are {possible_fresh_count} IDs considered fresh.")
 
 
 def read_input(file):
@@ -32,11 +40,23 @@ def parse_input(input):
     line_nr = 0
     fresh_ranges = []
     ingredients = []
+
+    lowest_id = +Infinity
+    highest_id = -Infinity
+
     while input[line_nr].strip() != '':
         range_line = input[line_nr]
         parsed_range = parse_range(range_line)
+
+        # Added for Part 2
+        lowest_id = min(lowest_id, parsed_range.start)
+        highest_id = max(highest_id, parsed_range.end)
+
         fresh_ranges += [parsed_range]
         line_nr += 1
+
+    if PUZZLE_NR == 2:
+        return fresh_ranges, None, lowest_id, highest_id
 
     line_nr += 1 # ignore empty line between blocks
 
@@ -45,7 +65,7 @@ def parse_input(input):
         ingredients += [int(ingredient_id)]
         line_nr += 1
 
-    return fresh_ranges, ingredients
+    return fresh_ranges, ingredients, lowest_id, highest_id
 
 
 def parse_range(range_str):
@@ -70,6 +90,48 @@ def test_ingredient(fresh_ranges: List[FreshRange], ingredient):
 
     print(f"Ingredient ID {ingredient} is spoiled.")
     return False
+
+
+def count_fresh_ids_starting_from(fresh_ranges: List[FreshRange], start_ingredient):
+    it_list = fresh_ranges.copy()
+    for fresh_range in it_list: # For Step 2 replaced fresh_ranges with a copy to be able to remove from passed list
+        if fresh_range.is_id_fresh(start_ingredient):
+            fresh_count = fresh_range.end - start_ingredient + 1
+            print(f"Ingredient ID {start_ingredient} and all {fresh_count} ingredients of range {fresh_range.start}-{fresh_range.end} are fresh.")
+            return fresh_count
+        else:
+            if start_ingredient > fresh_range.end:
+                fresh_ranges.remove(fresh_range)
+
+    print(f"Ingredient ID {start_ingredient} is spoiled.")
+    return 0
+
+
+def count_fresh_ids(fresh_ranges: List[FreshRange], lowest_id, highest_id):
+    ingredient_id = lowest_id
+    fresh_count = 0
+    total_range_count = highest_id-lowest_id
+    while ingredient_id <= highest_id:
+
+        range_fresh_count = count_fresh_ids_starting_from(fresh_ranges, ingredient_id)
+        if range_fresh_count > 0:
+            fresh_count += range_fresh_count
+            ingredient_id += range_fresh_count
+        else:
+            ingredient_id = get_smallest_id_of_range(fresh_ranges)
+
+        perc = (ingredient_id-lowest_id)/total_range_count
+        perc_text = "["+("="*int(perc*100))+(" "*int((1-perc)*100))+"]"
+        print(perc_text)
+    return fresh_count
+
+
+def get_smallest_id_of_range(fresh_ranges: List[FreshRange]):
+    result = Infinity
+    for fresh_range in fresh_ranges:
+        result = min(result, fresh_range.start)
+    return result
+
 
 if __name__ == '__main__':
     main()
